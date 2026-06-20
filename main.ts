05ca8db281d63376fcf47e9851803c9977e21a7e
KuchMaster <greykuchen@gmail.com>
2026-06-20 17:23:19 -0600

    Delete main.ts

--- main.ts
+++
@@ -1,1 +0,0 @@
-import { serveDir } from "jsr:@std/http/file-server"; Deno.serve((req) => serveDir(req, { fsRoot: ".", // This tells it to look in your main folder for files showDirListing: true, }));
